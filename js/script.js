f9388b64509f33e4c8f08a030ab97ca1cea4b883
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-21 16:40:36 +0200

    test 1

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+// Dato un array di oggetti letterali con:
+//  - url dell’immagine
+//  - titolo
+//  - descrizione
+// Creare un carosello come nella foto allegata.
+
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c.jpg',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
+
+const caroselloHtml = document.querySelector('.carosello');
+
+// Per stampare ogni slide
+function printSlide(){
+    const carosello = document.createElement('div');
+    images.forEach((nation) => {
+        const template = printTemplate(nation);
+        carosello.innerHTML += template;
+    });
+    caroselloHtml.appendChild(carosello);
+};
+printSlide();
+
+// Crea il contenitore della slide
+function printTemplate(nation){
+    const templateHtml = `
+        <div class="carousel-inner">
+            <h1>${nation.title}</h1>  
+            <img src="${nation.url}" class="w-100">    
+        </div> 
+    `;
+    return templateHtml;
+};
